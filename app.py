1975845e9f70a187a016d447511e3f98d94e85f8
unriellistic <charliewindle@gmail.com>
2020-12-10 22:38:32 +0800

    added signup and login forms

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
 from werkzeug.security import generate_password_hash, check_password_hash
 
 # general flask app setup
@@ -30,6 +33,20 @@ class User(UserMixin, db.Model):
 def load_user(user_id):
     return User.get(user_id)
 
+# forms
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[Email(message="Please enter a valid email."), DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8, message="Please choose a longer password.")])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password', message="Passwords must match.")])
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField("Log In")
+
+# loading pages
 @app.route('/')
 def index():
     return render_template('index.html')
